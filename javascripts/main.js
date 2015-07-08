43ae740d097fcc682b7016a33700bba0d38d336e
Simon Beal <sb48@sanger.ac.uk>
2015-07-08 13:18:07 +0100

    Start dynamic conversion of readme in master Using showdown (https://github.com/showdownjs/showdown) Converts https://github.com/cancerit/cgpPindel/blob/master/README.md to html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+window.addEvent("domready",function(){
+	var readmeRequest = new Request({
+		url:"https://github.com/cancerit/cgpPindel/blob/master/README.md",
+    	onSuccess:function(response){
+    		alert("Success");
+    		var converter = new showdown.Converter(),
+    		text = '#hello, markdown!',
+    		html = converter.makeHtml(text);
+			alert(html);
+    	}
+    }).send(); //Don't forget to send our request!
+});
+
+
