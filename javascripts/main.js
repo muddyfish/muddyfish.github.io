45889ad39148d036a713c8d035de3cfb1995296e
Simon Beal <sb48@sanger.ac.uk>
2015-07-13 11:42:16 +0100

    Update javascript so it alerts the user if using fail-over github api

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,7 @@ var current_project = location.pathname.split("/")[1];
 var corsproxy = "http://crossorigin.me/";
 var github_url = "https://github.com/cancerit/".concat(current_project, "/raw/master/README.md");
 var final_url = corsproxy.concat(github_url);
+var error_html = "<small>Using fail-over page generation, please notify <a href='mailto:cgp-it@sanger.ac.uk' target='_top'>cgp-it@sanger.ac.uk</a></small><br>";
 $.get(final_url)
     .success(function (response) {
 	    var converter = new showdown.Converter(),
@@ -14,8 +15,8 @@ $.get(final_url)
 	    xhr.setRequestHeader("Accept", "application/vnd.github.3.html");
 	    xhr.setRequestHeader("User-Agent", "CancerIT")
 		xhr.send();
-
+	    
 	    xhr.onload = function(e){
-		$('#content').replaceWith(xhr.response);
+		$('#content').replaceWith(error_html.concat(xhr.response));
 	    }
 	});
