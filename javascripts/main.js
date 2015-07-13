0db1ada45847a7b5f4cc609dab094e34902bbce0
Simon Beal <sb48@sanger.ac.uk>
2015-07-13 11:25:48 +0100

    Replace github api with cors proxy Add showdown Cors proxy site is at crossorigin.me If proxy site is down, go back to the github api Dynamically load the correct readme file based on the path     Gets the readme based on first pathname (/RETURNED/stuff/stuff.html)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,21 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.github.com/repos/cancerit/cgpPindel/readme?ref=master');
-xhr.setRequestHeader("Accept", "application/vnd.github.3.html");
-xhr.setRequestHeader("User-Agent", "CancerIT")
-xhr.send();
+var current_project = location.pathname.split("/")[1];
+var corsproxy = "http://crossorigin.me/";
+var github_url = "https://github.com/cancerit/".concat(current_project, "/raw/master/README.md");
+var final_url = corsproxy.concat(github_url);
+$.get(final_url)
+    .success(function (response) {
+	    var converter = new showdown.Converter(),
+		html = converter.makeHtml(response);
+	    $("#content").html(html);
+	})
+    .error(function () {
+	    var xhr = new XMLHttpRequest();
+	    xhr.open('GET', 'https://api.github.com/repos/cancerit/'.concat(current_project, '/readme?ref=master'));
+	    xhr.setRequestHeader("Accept", "application/vnd.github.3.html");
+	    xhr.setRequestHeader("User-Agent", "CancerIT")
+		xhr.send();
 
-xhr.onload = function(e){
-    $('#content').replaceWith(xhr.response);
-}
+	    xhr.onload = function(e){
+		$('#content').replaceWith(xhr.response);
+	    }
+	});
